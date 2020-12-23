74583d955d1757323477e38390a22ccea326ce37
Dezajno <dezajno@junk.studio>
2020-12-23 14:40:54 +0100

    Move utilities to util module

--- src/lib.rs
+++ src/lib.rs
@@ -1,156 +1,24 @@
 extern crate proc_macro;
 
+mod util;
+
 use std::collections::HashMap;
-use std::hash::Hash;
 
 use syn::parse_macro_input;
 use syn::DeriveInput;
 use syn::Data;
 use syn::Meta;
-use syn::MetaList;
 use syn::NestedMeta;
-use syn::MetaNameValue;
 use syn::Fields;
 use syn::Type;
 use syn::Path;
 use syn::Ident;
 use quote::quote;
-use quote::quote_spanned;
 use proc_macro::TokenStream;
 
-fn error(spanned: &impl syn::spanned::Spanned, message: &str) -> TokenStream {
-	let span = spanned.span();
-	let output = quote_spanned! {
-		span => compile_error!(#message);
-	};
-
-	output.into()
-}
-
-enum AttrArgsError<'attr, 'ident, I: ?Sized> {
-	KeyMismatch {
-		specified: &'attr syn::Path,
-		required: &'ident I
-	},
-	InvalidArg(syn::Path),
-	LitArg(syn::Lit),
-	DupliateArg(syn::Path),
-	NoMeta(&'attr syn::Attribute),
-	NoNestedMeta(MetaNameValue)
-}
-
-impl<'attr, 'ident, I: ?Sized> AttrArgsError<'attr, 'ident, I> {
-	fn explain(self) -> TokenStream where I: std::fmt::Display {
-		match self {
-			Self::KeyMismatch{ specified, required } => error(specified, &format!("Expected '{}'.", required)),
-			Self::InvalidArg(key) => error(&key, "Invalid argument."),
-			Self::LitArg(lit) => error(&lit, "Invalid first-level literal."),
-			Self::DupliateArg(key) => error(&key, "Duplicate argument."),
-			Self::NoMeta(attr) => error(attr, "Must be of 'meta' type."),
-			Self::NoNestedMeta(mnv) => error(&mnv, "Invalid name-value pair. Expected path or list."),
-		}
-	}
-}
-
-fn attr_args<'attr, 'ident, I: ?Sized>(
-	attrs: &'attr [syn::Attribute],
-	required_key: &'ident I,
-	known_arg_keys: &[&'ident I]
-) -> Result<HashMap<&'ident I, Vec<NestedMeta>>, AttrArgsError<'attr, 'ident, I>> where
-	syn::Ident: PartialEq<I>,
-	I: Hash + Eq
-{
-	let mut args: HashMap<&'ident I, Vec<NestedMeta>> = HashMap::new();
-
-	for attr in attrs {
-		if let Some(specified_key) = attr.path.get_ident() {
-			if specified_key != required_key {
-				return Err(AttrArgsError::KeyMismatch { 
-					specified: &attr.path,
-					required: required_key
-				})
-			}
-		} else {
-			return Err(AttrArgsError::KeyMismatch { 
-				specified: &attr.path,
-				required: required_key
-			})
-		}
-		
-		match attr.parse_meta() {
-			Ok(meta) => {
-				match meta {
-					Meta::NameValue(mnv) => {
-						return Err(AttrArgsError::NoNestedMeta(mnv))
-					}
-					Meta::Path(_) => {
-						// no arguments
-					},
-					Meta::List(MetaList {
-						nested,
-						..
-					}) => {
-
-						for nested_arg in nested {
-							
-							match nested_arg {
-								NestedMeta::Lit(lit) => {
-									return Err(AttrArgsError::LitArg(lit))
-								},
-								NestedMeta::Meta(meta) => {
-									let path = {
-										match &meta {
-											Meta::Path(path) => path,
-											Meta::List(list) => &list.path,
-											Meta::NameValue(name_value) => &name_value.path
-										}.clone()
-									};
-
-									let known_arg_key = {
-										if let Some(path_ident) = path.get_ident() {
-											if let Some(known_arg_key) = known_arg_keys.iter().find(|arg| &&path_ident == arg) {
-												known_arg_key
-											} else {
-												return Err(AttrArgsError::InvalidArg(path))
-											}
-										} else {
-											return Err(AttrArgsError::InvalidArg(path))
-										}
-									};
-			
-									if args.contains_key(known_arg_key) {
-										return Err(AttrArgsError::DupliateArg(path))
-									}
-
-									match meta {
-										Meta::Path(_) => {
-											args.insert(known_arg_key, vec![ ]);
-										},
-										Meta::List(list) => {
-											let nesteds = list.nested.into_iter().collect();
-											args.insert(known_arg_key, nesteds);
-										},
-										Meta::NameValue(name_value) => {
-											args.insert(known_arg_key, vec![
-												NestedMeta::Lit(name_value.lit)
-											]);
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			},
-			_ => {
-				return Err(AttrArgsError::NoMeta(attr))
-			}
-		}
-	}
-
-	Ok(args)
-}
-
+use util::error;
+use util::attr_args;
+use util::AttrArgsError;
 
 #[proc_macro_derive(CompositeError, attributes(compound_error))]
 pub fn derive_composite_error(input: TokenStream) -> TokenStream {
---
+++ src/util.rs
@@ -0,0 +1,147 @@
+
+use std::collections::HashMap;
+use std::hash::Hash;
+
+use proc_macro::TokenStream;
+
+use quote::quote_spanned;
+
+use syn::Meta;
+use syn::MetaList;
+use syn::MetaNameValue;
+use syn::NestedMeta;
+
+pub fn error(spanned: &impl syn::spanned::Spanned, message: &str) -> TokenStream {
+	let span = spanned.span();
+	let output = quote_spanned! {
+		span => compile_error!(#message);
+	};
+
+	output.into()
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub enum AttrArgsError<'attr, 'ident, I: ?Sized> {
+	KeyMismatch {
+		specified: &'attr syn::Path,
+		required: &'ident I
+	},
+	InvalidArg(syn::Path),
+	LitArg(syn::Lit),
+	DupliateArg(syn::Path),
+	NoMeta(&'attr syn::Attribute),
+	NoNestedMeta(MetaNameValue)
+}
+
+impl<'attr, 'ident, I: ?Sized> AttrArgsError<'attr, 'ident, I> {
+	pub fn explain(self) -> TokenStream where I: std::fmt::Display {
+		match self {
+			Self::KeyMismatch{ specified, required } => error(specified, &format!("Expected '{}'.", required)),
+			Self::InvalidArg(key) => error(&key, "Invalid argument."),
+			Self::LitArg(lit) => error(&lit, "Invalid first-level literal."),
+			Self::DupliateArg(key) => error(&key, "Duplicate argument."),
+			Self::NoMeta(attr) => error(attr, "Must be of 'meta' type."),
+			Self::NoNestedMeta(mnv) => error(&mnv, "Invalid name-value pair. Expected path or list."),
+		}
+	}
+}
+
+pub fn attr_args<'attr, 'ident, I: ?Sized>(
+	attrs: &'attr [syn::Attribute],
+	required_key: &'ident I,
+	known_arg_keys: &[&'ident I]
+) -> Result<HashMap<&'ident I, Vec<NestedMeta>>, AttrArgsError<'attr, 'ident, I>> where
+	syn::Ident: PartialEq<I>,
+	I: Hash + Eq
+{
+	let mut args: HashMap<&'ident I, Vec<NestedMeta>> = HashMap::new();
+
+	for attr in attrs {
+		if let Some(specified_key) = attr.path.get_ident() {
+			if specified_key != required_key {
+				return Err(AttrArgsError::KeyMismatch { 
+					specified: &attr.path,
+					required: required_key
+				})
+			}
+		} else {
+			return Err(AttrArgsError::KeyMismatch { 
+				specified: &attr.path,
+				required: required_key
+			})
+		}
+		
+		match attr.parse_meta() {
+			Ok(meta) => {
+				match meta {
+					Meta::NameValue(mnv) => {
+						return Err(AttrArgsError::NoNestedMeta(mnv))
+					}
+					Meta::Path(_) => {
+						// no arguments
+					},
+					Meta::List(MetaList {
+						nested,
+						..
+					}) => {
+
+						for nested_arg in nested {
+							
+							match nested_arg {
+								NestedMeta::Lit(lit) => {
+									return Err(AttrArgsError::LitArg(lit))
+								},
+								NestedMeta::Meta(meta) => {
+									let path = {
+										match &meta {
+											Meta::Path(path) => path,
+											Meta::List(list) => &list.path,
+											Meta::NameValue(name_value) => &name_value.path
+										}.clone()
+									};
+
+									let known_arg_key = {
+										if let Some(path_ident) = path.get_ident() {
+											if let Some(known_arg_key) = known_arg_keys.iter().find(|arg| &&path_ident == arg) {
+												known_arg_key
+											} else {
+												return Err(AttrArgsError::InvalidArg(path))
+											}
+										} else {
+											return Err(AttrArgsError::InvalidArg(path))
+										}
+									};
+			
+									if args.contains_key(known_arg_key) {
+										return Err(AttrArgsError::DupliateArg(path))
+									}
+
+									match meta {
+										Meta::Path(_) => {
+											args.insert(known_arg_key, vec![ ]);
+										},
+										Meta::List(list) => {
+											let nesteds = list.nested.into_iter().collect();
+											args.insert(known_arg_key, nesteds);
+										},
+										Meta::NameValue(name_value) => {
+											args.insert(known_arg_key, vec![
+												NestedMeta::Lit(name_value.lit)
+											]);
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			},
+			_ => {
+				return Err(AttrArgsError::NoMeta(attr))
+			}
+		}
+	}
+
+	Ok(args)
+}
+
